4c5cdcba927d160ce3f96be27b3e40a929f323d4
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-04-11 14:46:24 +0300

    Update UserController.java

--- src/main/java/nur/Controller/UserController.java
+++ src/main/java/nur/Controller/UserController.java
@@ -5,12 +5,12 @@ import nur.service.UserServ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
-import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
 
 
 @RestController
